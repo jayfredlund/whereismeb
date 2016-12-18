fa265a73d287b211cf7cd40027ed45501dac892c
Jay Fredlund <jay.fredlund@kotisdesign.com>
2016-12-18 00:34:46 -0800

    adds randomized meb image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,12 @@ class ApplicationController < ActionController::Base
                          "Nashville", "San Diego", "Seattle", "Chicago", "Dublin", "Philadelphia",
                          "San Jose", "Brooklyn", "Denver", "Los Angeles", "Las Vegas", "San Antonio"]
 
+  MEB_IMAGE_PATHS = ["meb-boston.jpg", "meb-creek.jpg", "meb-face.jpg", "meb-flag.jpg", "meb-meat.jpg",
+                     "meb-medals.jpg", "meb-subway.jpg", "mebcake.jpg"]
+
   def hi_carolyn
     @location = AVAILABLE_LOCATIONS.sample
+    @image = MEB_IMAGE_PATHS.sample
     render 'layouts/hi_carolyn'
   end
 
