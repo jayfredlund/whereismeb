0bdf57ef7b4ea3fdad84982c1e5b9b636369216d
Jay Fredlund <jay.fredlund@kotisdesign.com>
2016-12-18 00:11:34 -0800

    adds mebs face and random location

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  AVAILABLE_LOCATIONS = ["Arizona", "New Orleans", "DC", "Dallas", "San Francisco", "Raleigh",
+                         "Nashville", "San Diego", "Seattle", "Chicago", "Dublin", "Philadelphia",
+                         "San Jose", "Brooklyn", "Denver", "Los Angeles", "Las Vegas", "San Antonio"]
+
   def hi_carolyn
+    @location = AVAILABLE_LOCATIONS.sample
     render 'layouts/hi_carolyn'
   end
 
